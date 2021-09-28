a0df7f2023c37991f5902d33145da01841aadaca
김예림 <bam03196@naver.com>
2021-09-28 23:22:19 +0900

    making login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="back">     
+        <b-container fluid class="back">
+            <div>
+                <b-form-group
+                id="fieldset-horizontal"
+                label-cols-sm="4"
+                label-cols-lg="3"
+                content-cols-sm
+                content-cols-lg="7"
+                label="아이디"
+                label-for="input-horizontal"
+                >
+                <b-form-input id="input-horizontal"></b-form-input>
+
+                </b-form-group>
+                <b-form-group
+                id="fieldset-horizontal"
+                label-cols-sm="4"
+                label-cols-lg="3"
+                content-cols-sm
+                content-cols-lg="7"
+                label="비밀번호"
+                label-for="input-horizontal"
+                >
+                <b-form-input id="input-horizontal"></b-form-input>
+                </b-form-group>
+
+                <div>
+                <b-button block variant="primary">Block Level Button</b-button>
+                </div>
+            </div>
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {
+            src : "../assets/img/background.png",
+        }
+    },
+}
+</script>
+
+<style>
+.back{
+    background-image: url("../assets/img/background.png");
+}
+</style>
